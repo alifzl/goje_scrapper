e87f1e3a14c9cf36c55966f38d0bcae5592845e3
Ali Fazeli <alifazeli@noay.ir>
2021-04-24 17:00:32 +0430

    goje reside

---
+++ tests/test_extract_audience_reviews.py
@@ -0,0 +1,8 @@
+from goje_scrapper.goje import *
+
+movie_url = 'https://www.rottentomatoes.com/m/a_separation_2011'
+
+movie_scraper = GojeScraper(movie_url=movie_url)
+
+all_reviews = movie_scraper.extract_all_audience_reviews(page_number=3)
+print(all_reviews)
